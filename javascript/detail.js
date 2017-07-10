5c522bec7e3ce82d4d000829000a7ff9f48d44cb
lxddup <546856874@qq.com>
2017-07-10 15:49:52 +0800

    订单详情页面

---
+++ javascript/detail.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/7/10 0010.
+ */
+
+
+$(document).on("pageinit",function () {
+    $("#left img").on("tap",function () {
+        location.href = "index1.html";
+    })
+})
